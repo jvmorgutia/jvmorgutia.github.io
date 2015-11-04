2e68142f68098abe95ebdd5c80562ab477a8f3f2
Jesse <jesse@Jesses-MBP.home>
2015-11-03 19:19:16 -0800

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//jQuery for page scrolling feature
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 800, 'easeOutCubic');
+        event.preventDefault();
+    });
+});
